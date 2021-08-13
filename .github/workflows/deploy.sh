be0e8bb9304560631674d6c8f1c9db8be22d8d1a
Hari G <ghari8118@gmail.com>
2021-08-13 21:04:54 +0530

    chore: replace on-push deploy with manual trigger

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,26 +1,3 @@
-tag=$(git describe --tags --abbrev=0)
-echo "latest tag: $tag"
-commits=$(git log --pretty="%h" "$tag"...HEAD)
-echo $commits
-mapfile -t commitList <<< "$commits"
-
-regex="^(fix|feat):"
-shouldDeploy=false
-for i in "${commitList[@]}"; do
-  sub=$(git log --pretty="%s" -1 "$i")
-  echo "commit: $sub"
-  if [[ "$sub" =~ $regex ]]; then
-    shouldDeploy=true
-    break
-  fi
-done
-
-echo "Need to Deploy: $shouldDeploy"
-if [ $shouldDeploy == false ]; then
-  echo 'exiting..'
-  exit 0
-fi
-
 git config user.name "Hari G"
 git config user.email "ghari81189@gmail.com"
 
